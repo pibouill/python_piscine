3eb62239cf68c75d964f982a4cee62d65d3c4ba9
pibouill <pibouill@student.42prague.com>
2024-09-05 18:04:02 +0200

    saving

---
+++ cell07/ex03/persons_of_interest.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    persons_of_interest.py                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/09/05 17:58:59 by pibouill          #+#    #+#              #
+#    Updated: 2024/09/05 17:58:59 by pibouill         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	famous_births(dic):
+	sorted = []
+	for name, smoll_dic in dic:
+
+
+
+
+	
+women_scientists = {
+"ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+"cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+"lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+"grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+famous_births(women_scientists)
+
