760a1f169cac7c18e33f66d51d3ab17b5aa94aa7
pibouill <pibouill@student.42prague.com>
2024-09-06 14:57:38 +0200

    FINISHED

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -11,19 +11,18 @@
 #                                                                              #
 # **************************************************************************** #
 
-def	famous_births(dic):
-	sorted = []
-	for name, smoll_dic in dic:
-
-
-
-
-	
 women_scientists = {
 "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
 "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
 "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
 "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
 }
-famous_births(women_scientists)
 
+def	famous_births(dic):
+
+	sorted_scientists = sorted(women_scientists.items(), key=lambda item: item[1]["date_of_birth"])
+	for key, value in sorted_scientists:
+		print(value["name"], "is a great scientist born in", value["date_of_birth"] + '.')
+
+
+famous_births(women_scientists)
