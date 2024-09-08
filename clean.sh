f710a5c2a340146299e34fc7920fb8d592ceed04
pibouill <pibouill@student.42prague.com>
2024-09-08 18:56:51 +0200

    cleaning file system

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Move all subdirectory files to the current directory
-find . -mindepth 2 -type f -exec mv -t . {} +
-
-# Remove all empty subdirectories
-find . -mindepth 1 -type d -empty -exec rmdir {} +
-
